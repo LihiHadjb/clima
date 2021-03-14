3e711ae070f649f7628fe0474543092d30d22f5a
LihiHadjb <48348862+LihiHadjb@users.noreply.github.com>
2021-03-14 02:51:29 +0200

    test query

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import Flask
-from db import init_db
+from psycopg2 import Error
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import json
+
 
 app = Flask(__name__)
 
+try:
+
+    connection = psycopg2.connect(host="ec2-52-71-161-140.compute-1.amazonaws.com",
+                                  dbname="d8a5obfa9du048",
+                                  user="nobeehmaarddfb",
+                                  password="15956bfd9f88359095c6850be56b5e8ed030d15bdb8449a50834f90fe718a70a")
+except (Exception, Error) as error:
+    print("Connecting to PostgreSQL failed!", error)
+
+cursor = connection.cursor(cursor_factory=RealDictCursor)
+
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    cursor.execute("select * from forecasts")
+    return json.dumps(cursor.fetchall(), indent=2)
+
 
 
 if __name__ == '__main__':
