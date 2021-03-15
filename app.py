ed1be781f26284a17237ac2ee36844dec69d28ce
LihiHadjb <48348862+LihiHadjb@users.noreply.github.com>
2021-03-15 05:07:02 +0200

    test

--- app.py
+++ app.py
@@ -9,21 +9,21 @@ init = DBInitializer()
 init.create_table()
 init.load_data("data")
 
-retriver = Retriever()
+retriever = Retriever()
 
 
 @app.route('/weather/data')
 def data():
     lat = float(request.args.get('lat'))
     lon = float(request.args.get('lon'))
-    return jsonify(retriver.get_data_for_location(lat, lon))
+    return jsonify(retriever.get_data_for_location(lat, lon))
 
 
 @app.route('/weather/summarize')
 def summarize():
     lat = float(request.args.get('lat'))
     lon = float(request.args.get('lon'))
-    return jsonify(retriver.get_summary_for_location(lat, lon))
+    return jsonify(retriever.get_summary_for_location(lat, lon))
 
 
 if __name__ == '__main__':
