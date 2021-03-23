35dbf3bb3f3fd4ec17085d172b0905c703bbef22
LihiHadjb <48348862+LihiHadjb@users.noreply.github.com>
2021-03-23 22:44:25 +0200

    Revert "json"
    This reverts commit 382c18a8

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 from retriever import Retriever
 from flask import jsonify
-import json
 
 app = Flask(__name__)
 retriever = Retriever()
@@ -17,7 +16,8 @@ def data():
 def summarize():
     lat = float(request.args.get('lat'))
     lon = float(request.args.get('lon'))
-    return jsonify(json.loads(retriever.get_summary_for_location(lat, lon)))
+    return jsonify(retriever.get_summary_for_location(lat, lon))
+
 
 if __name__ == '__main__':
     app.run()
