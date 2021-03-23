382c18a836a3291c58d8f2fe17f718353deff494
LihiHadjb <48348862+LihiHadjb@users.noreply.github.com>
2021-03-23 22:39:03 +0200

    json

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from retriever import Retriever
 from flask import jsonify
+import json
 
 app = Flask(__name__)
 retriever = Retriever()
@@ -16,8 +17,7 @@ def data():
 def summarize():
     lat = float(request.args.get('lat'))
     lon = float(request.args.get('lon'))
-    return jsonify(retriever.get_summary_for_location(lat, lon))
-
+    return jsonify(json.loads(retriever.get_summary_for_location(lat, lon)))
 
 if __name__ == '__main__':
     app.run()
